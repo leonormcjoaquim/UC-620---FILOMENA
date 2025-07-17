073811b7f4dfa1db0fca9b5f4e482f05e9d2ebea
Leonor Joaquim <leonorjoaquim98@gmail.com>
2025-07-17 21:05:02 +0100

    treino resolvido e feito
    afafs

--- Projeto 620/FORMS/treino.Designer.cs
+++ Projeto 620/FORMS/treino.Designer.cs
@@ -28,7 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(treino));
             this.btn_sair = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pb_menu = new System.Windows.Forms.PictureBox();
+            this.pn_opcoes = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_home = new System.Windows.Forms.Button();
+            this.btn_appoitments = new System.Windows.Forms.Button();
+            this.btn_workouts = new System.Windows.Forms.Button();
+            this.btn_food = new System.Windows.Forms.Button();
+            this.btn_pt = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bt_logout = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.sidebarTransition = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_menu)).BeginInit();
+            this.pn_opcoes.SuspendLayout();
             this.SuspendLayout();
             // 
             // btn_sair
@@ -41,15 +60,229 @@
             this.btn_sair.UseVisualStyleBackColor = true;
             this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.pn_opcoes);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1000, 601);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel3.Controls.Add(this.pb_menu);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1000, 56);
+            this.panel3.TabIndex = 8;
+            // 
+            // pb_menu
+            // 
+            this.pb_menu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pb_menu.Image = ((System.Drawing.Image)(resources.GetObject("pb_menu.Image")));
+            this.pb_menu.Location = new System.Drawing.Point(3, 11);
+            this.pb_menu.Name = "pb_menu";
+            this.pb_menu.Padding = new System.Windows.Forms.Padding(15, 5, 0, 0);
+            this.pb_menu.Size = new System.Drawing.Size(51, 35);
+            this.pb_menu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_menu.TabIndex = 8;
+            this.pb_menu.TabStop = false;
+            this.pb_menu.Click += new System.EventHandler(this.pb_menu_Click);
+            // 
+            // pn_opcoes
+            // 
+            this.pn_opcoes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.pn_opcoes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pn_opcoes.Controls.Add(this.btn_home);
+            this.pn_opcoes.Controls.Add(this.btn_appoitments);
+            this.pn_opcoes.Controls.Add(this.btn_workouts);
+            this.pn_opcoes.Controls.Add(this.btn_food);
+            this.pn_opcoes.Controls.Add(this.btn_pt);
+            this.pn_opcoes.Controls.Add(this.button2);
+            this.pn_opcoes.Controls.Add(this.bt_logout);
+            this.pn_opcoes.Controls.Add(this.btn_exit);
+            this.pn_opcoes.Location = new System.Drawing.Point(0, 59);
+            this.pn_opcoes.Name = "pn_opcoes";
+            this.pn_opcoes.Size = new System.Drawing.Size(50, 496);
+            this.pn_opcoes.TabIndex = 7;
+            // 
+            // btn_home
+            // 
+            this.btn_home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_home.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_home.FlatAppearance.BorderSize = 2;
+            this.btn_home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_home.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_home.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_home.Image = ((System.Drawing.Image)(resources.GetObject("btn_home.Image")));
+            this.btn_home.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_home.Location = new System.Drawing.Point(0, 0);
+            this.btn_home.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.btn_home.Name = "btn_home";
+            this.btn_home.Size = new System.Drawing.Size(318, 53);
+            this.btn_home.TabIndex = 7;
+            this.btn_home.Text = "Home";
+            this.btn_home.UseVisualStyleBackColor = false;
+            // 
+            // btn_appoitments
+            // 
+            this.btn_appoitments.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_appoitments.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_appoitments.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_appoitments.FlatAppearance.BorderSize = 2;
+            this.btn_appoitments.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_appoitments.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_appoitments.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_appoitments.Image = ((System.Drawing.Image)(resources.GetObject("btn_appoitments.Image")));
+            this.btn_appoitments.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_appoitments.Location = new System.Drawing.Point(0, 61);
+            this.btn_appoitments.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.btn_appoitments.Name = "btn_appoitments";
+            this.btn_appoitments.Size = new System.Drawing.Size(318, 50);
+            this.btn_appoitments.TabIndex = 7;
+            this.btn_appoitments.Text = "Especialista";
+            this.btn_appoitments.UseVisualStyleBackColor = false;
+            // 
+            // btn_workouts
+            // 
+            this.btn_workouts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_workouts.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_workouts.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_workouts.FlatAppearance.BorderSize = 2;
+            this.btn_workouts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_workouts.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_workouts.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_workouts.Image = ((System.Drawing.Image)(resources.GetObject("btn_workouts.Image")));
+            this.btn_workouts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_workouts.Location = new System.Drawing.Point(0, 119);
+            this.btn_workouts.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.btn_workouts.Name = "btn_workouts";
+            this.btn_workouts.Size = new System.Drawing.Size(318, 53);
+            this.btn_workouts.TabIndex = 7;
+            this.btn_workouts.Text = "Exercicios";
+            this.btn_workouts.UseVisualStyleBackColor = false;
+            // 
+            // btn_food
+            // 
+            this.btn_food.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_food.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_food.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_food.FlatAppearance.BorderSize = 2;
+            this.btn_food.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_food.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_food.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_food.Image = ((System.Drawing.Image)(resources.GetObject("btn_food.Image")));
+            this.btn_food.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_food.Location = new System.Drawing.Point(0, 180);
+            this.btn_food.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.btn_food.Name = "btn_food";
+            this.btn_food.Size = new System.Drawing.Size(318, 53);
+            this.btn_food.TabIndex = 7;
+            this.btn_food.Text = "Comida";
+            this.btn_food.UseVisualStyleBackColor = false;
+            // 
+            // btn_pt
+            // 
+            this.btn_pt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_pt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_pt.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_pt.FlatAppearance.BorderSize = 2;
+            this.btn_pt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_pt.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pt.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_pt.Image = ((System.Drawing.Image)(resources.GetObject("btn_pt.Image")));
+            this.btn_pt.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_pt.Location = new System.Drawing.Point(0, 241);
+            this.btn_pt.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.btn_pt.Name = "btn_pt";
+            this.btn_pt.Size = new System.Drawing.Size(318, 53);
+            this.btn_pt.TabIndex = 7;
+            this.btn_pt.Text = "Treino PT";
+            this.btn_pt.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(0, 302);
+            this.button2.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(318, 52);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Motivação";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // bt_logout
+            // 
+            this.bt_logout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.bt_logout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bt_logout.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
+            this.bt_logout.FlatAppearance.BorderSize = 2;
+            this.bt_logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_logout.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_logout.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.bt_logout.Image = ((System.Drawing.Image)(resources.GetObject("bt_logout.Image")));
+            this.bt_logout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bt_logout.Location = new System.Drawing.Point(0, 362);
+            this.bt_logout.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.bt_logout.Name = "bt_logout";
+            this.bt_logout.Size = new System.Drawing.Size(318, 53);
+            this.bt_logout.TabIndex = 7;
+            this.bt_logout.Text = "Logout";
+            this.bt_logout.UseVisualStyleBackColor = false;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_exit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.btn_exit.FlatAppearance.BorderSize = 2;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_exit.Image = ((System.Drawing.Image)(resources.GetObject("btn_exit.Image")));
+            this.btn_exit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_exit.Location = new System.Drawing.Point(0, 423);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(318, 52);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            // 
+            // sidebarTransition
+            // 
+            this.sidebarTransition.Interval = 10;
+            this.sidebarTransition.Tick += new System.EventHandler(this.sidebarTransition_Tick);
+            // 
             // treino
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_sair);
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "treino";
             this.Text = "treino";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_menu)).EndInit();
+            this.pn_opcoes.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -57,5 +290,18 @@
         #endregion
 
         private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel pn_opcoes;
+        private System.Windows.Forms.Button btn_home;
+        private System.Windows.Forms.Button btn_appoitments;
+        private System.Windows.Forms.Button btn_workouts;
+        private System.Windows.Forms.Button btn_food;
+        private System.Windows.Forms.Button btn_pt;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bt_logout;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pb_menu;
+        private System.Windows.Forms.Timer sidebarTransition;
     }
 }
--- Projeto 620/FORMS/treino.cs
+++ Projeto 620/FORMS/treino.cs
@@ -23,5 +23,35 @@ namespace Projeto_620.FORMS
             login Login = new login();
             Login.Show();
         }
+
+        private void pb_menu_Click(object sender, EventArgs e)
+        {
+            sidebarTransition.Start();
+        }
+
+        bool sidebarExpand = false;
+
+        private void sidebarTransition_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                pn_opcoes.Width -= 10;
+                // SE O NOME FOR MAIOR QUE AQUELE FODE O RESTO
+                if (pn_opcoes.Width <= 45)
+                {
+                    sidebarExpand = false;
+                    sidebarTransition.Stop();
+                }
+            }
+            else
+            {
+                pn_opcoes.Width += 10;
+                if (pn_opcoes.Width >= 245)
+                {
+                    sidebarExpand = true;
+                    sidebarTransition.Stop();
+                }
+            }
+        }
     }
 }
