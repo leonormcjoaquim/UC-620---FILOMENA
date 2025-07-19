2010bd511b2fcdaf778524bd4e061c2baf21ef64
André Correia <andrecorreis@gmail.com>
2025-07-19 20:26:27 +0100

    Continuar user + atribuir treino
    o que esta no comentario

--- Projeto 620/models/Exercicio.cs
+++ Projeto 620/models/Exercicio.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Projeto_620.models
 {
     internal class Exercicio
     {
+
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public int Duracao { get; set; }
+        public int CaloriasQueimadas { get; set; }
+        public DateTime Data {  get; set; }
+
+        public Exercicio(string nomeTreino, string tipoExercicio, int duracaoExercicio, int caloriasQueimadas, DateTime dataTreino) 
+        { 
+            Nome = nomeTreino;
+            Tipo = tipoExercicio;
+            Duracao = duracaoExercicio;
+            CaloriasQueimadas = caloriasQueimadas;
+            Data = dataTreino;
+        }
+
+        public bool MetaCalorias (int metaCalorias)
+        {
+            int calorias = this.CaloriasQueimadas;
+            if (metaCalorias <= calorias)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Tipo} | {Duracao} min | {CaloriasQueimadas} kcal quemimadas na data {Data.ToString()}";
+        }
+
     }
 }
