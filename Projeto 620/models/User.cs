3636339a99084c3c78611562ceb632e8dd919384
André Correia <andrecorreis@gmail.com>
2025-07-19 19:51:22 +0100

    Inserir atributos ao user
    isso!

--- Projeto 620/models/User.cs
+++ Projeto 620/models/User.cs
@@ -8,5 +8,15 @@ namespace Projeto_620.models
 {
     internal class User
     {
+        public string nome {  get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public int idade { get; set; }
+        public int altura { get; set; }
+        public double peso { get; set; }
+        public List<Marcacao> Marcacao { get; set; }
+        public List<Alimentacao> Alimentacao { get; set; }
+        public List<Exercicio> Exercicio { get; set; }
+
     }
 }
